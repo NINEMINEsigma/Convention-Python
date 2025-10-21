11e1aa0f865599bbb0715a9a8b35fa1c8dc59887
ninemine <1371605831@qq.com>
2025-10-21 09:34:26 +0800

    Colorful独立

--- [Test]/test.py
+++ [Test]/test.py
@@ -3,8 +3,7 @@ import os
 from time import sleep
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from Convention.Runtime.File import *
+from Convention.Runtime.Config import *
 
-file = ToolFile("[Test]")|"temp"|None
-print(file.MustExistsPath())
+PrintColorful(ConsoleFrontColor.RED, "Hello, World!")
 
