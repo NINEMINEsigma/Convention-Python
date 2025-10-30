e56d78a7576028f34dac96884a4de134271bb1db
ninemine <1371605831@qq.com>
2025-10-30 20:59:01 +0800

    Add PrettyFace

--- [Test]/test.py
+++ [Test]/test.py
@@ -1,9 +1,7 @@
 import sys
 import os
-from time import sleep
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from Convention.Runtime.Config import *
-
-PrintColorful(ConsoleFrontColor.RED, "Hello, World!")
+print(PlatformIndicator.PrettyFace)
 
